5af454626fece26e0048ea2e3c4b1c77bf713e96
jessiezhu <jessie.zhu@nakedhub.cn>
2017-07-23 17:33:23 +0800

    add auth action creators

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import { Navigation } from 'components'
+import { connect } from 'react-redux'
+import { container, innerContainer } from './styles.css'
+
 import HomeContainer from '../Home/HomeContainer'
 class MainContainer extends Component {
+  propTypes: {
+    isAuthed: PropTypes.bool.isRequired
+  }
   render () {
     return (
-      <div>
-        <HomeContainer/>
+      <div className={container}>
+        <Navigation isAuthed={this.props.isAuthed} />
+        <div className={innerContainer}>
+          <HomeContainer/>
+        </div>
       </div>
     )
   }
 }
-
-export default MainContainer
+export default connect(
+  ({users}) => ({isAuthed: users.isAuthed})
+)(MainContainer)
