4c3ccbd9f754b960ae0b53287b820f6eecb86cc2
jessiezhu <jessie.zhu@nakedhub.cn>
2017-07-23 16:08:34 +0800

    add schema and combine redux with react-router

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,16 +2,13 @@ import React, {Component} from 'react'
 import { BrowserRouter, IndexRoute, Route, hashHistory } from 'react-router-dom'
 import { MainContainer, HomeContainer } from 'containers'
 
-class Routes extends Component {
-  render() {
-    return (
-      <BrowserRouter history={hashHistory}>
-        <div>
-          <Route path='/' component={MainContainer}>
-          </Route>
-        </div>
-      </BrowserRouter>
-    )
-  }
+export default function getRoutes (checkAuth, history) {
+  return (
+    <BrowserRouter history={hashHistory}>
+      <div>
+        <Route path='/' component={MainContainer}>
+        </Route>
+      </div>
+    </BrowserRouter>
+  )
 }
-export default Routes
--- app/index.js
+++ app/index.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Routes from 'config/routes'
+import getRoutes from 'config/routes'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { hashHistory } from 'react-router-dom'
+import { routerReducer, syncHistoryWithStore } from 'react-router-redux'
+import * as reducers from 'redux/modules'
+import { createBrowserHistory } from 'history';
+
+const store = createStore(combineReducers({...reducers, routing: routerReducer}), compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+))
+
+const history = syncHistoryWithStore(createBrowserHistory(), store)
+
+function checkAuth (nextState, replace) {
+  return true
+}
 
 ReactDOM.render(
-  <Routes/>,
+  <Provider store={store}>
+    {getRoutes(checkAuth, history)}
+  </Provider>,
   document.getElementById('app')
 )
